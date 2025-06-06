c993f60a8ed3b8181ee95d773c301cf5f534df56
ingJuanArenas <juanarenasing@gmail.com>
2025-06-06 09:30:55 -0500

    La clase principal para Personas, agrega los datos yse realiza las operaciones indicadas usando streams y sus funciones. Funciona correctamente.

--- src/PersonaMain.java
+++ src/PersonaMain.java
@@ -46,9 +46,69 @@ public class PersonaMain {
 
         
 
-         
+            System.out.println("Según los datos ingresados: ");
+            // contar
+            long cantidad = personas.stream().count();
+            System.out.println("La cantidad de personas registragas es = "  + cantidad);
 
 
+            //promedio
+
+            Double promedio= personas.stream().mapToDouble(Persona::getEdad).average().orElse(0);
+            System.out.println("El promedio de las edades de las personas registradas es: "+ promedio);
+
+            //  mayores de edad
+            long cantidadMayores= personas.stream().filter(p-> p.getEdad()>= 18).count();
+            System.out.println("Las cantidad de personas mayores de edad registradas son: "+ cantidadMayores);
+
+            // mostrar personas nombres empiecen con A
+            System.out.println("Las personas que comienzan su nombre con la letra A son: ");
+            personas.stream().filter(p -> p.getNombre().toUpperCase().startsWith("A")).forEach(p -> {
+                System.out.println("Nombre:  " + p.getNombre() + " Apellido: " + p.getApellido());
+            });;
+
+            // mostrar personas apellido m
+            System.out.println("Las personas que contienen M en su apellido son: ");
+            personas.stream().filter(p-> p.getApellido().toLowerCase().contains("m")).forEach(p->{
+                System.out.println("Nombre: "+p.getNombre()+ " Apellido: "+ p.getApellido());
+            });
+
+            //  sueldo de 8h con director masculino
+            System.out.println("Los directores y su sueldo de 8 horas: ");
+            personas.stream()
+            .filter(p -> p.getCargo().equalsIgnoreCase("director") && p.getGenero().equalsIgnoreCase("M"))
+            .peek(p -> System.out.print("Nombre: " + p.getNombre() + " " + p.getApellido() + " "))
+            .map(p -> p.getSueldoHora() * 8)
+            .forEach(sueldo -> System.out.println("Sueldo por 8 horas: $" + sueldo));
+
+            //Encontrar la desarrolladora mujer
+         Optional<Persona> primeraDesarrolladora = personas.stream()
+        .filter(p -> p.getCargo().equalsIgnoreCase("desarrollador") && p.getGenero().equalsIgnoreCase("F"))
+        .findFirst();
+
+         primeraDesarrolladora.ifPresent(p ->
+            System.out.println("Primera desarrolladora encontrada: " + p.getNombre() + " " + p.getApellido())
+            );
+
+        //Desarrollador que mas gana x hora
+        Optional<Persona> masGana = personas.stream()
+        .filter(p -> p.getCargo().equalsIgnoreCase("desarrollador"))
+        .max(Comparator.comparing(Persona::getSueldoHora));
+
+        if (masGana.isPresent()) {
+            Persona p = masGana.get();
+            System.out.println("Desarrollador que más gana: " + p.getNombre() + " " + p.getApellido() +
+                            " - $" + p.getSueldoHora() + " por hora");
+        }
+        // Mujeres ordenadas
+        System.out.println("Listado de mujeres ordenado");
+        personas.stream()
+        .filter(p -> p.getGenero().equalsIgnoreCase("F"))
+        .sorted(Comparator.comparing(Persona::getNombre))
+        .forEach(p -> System.out.println(p.getNombre() + " " + p.getApellido()));
+
+
+        sc.close();
 
     }
 }
